3fc477a2a41b7035af0702abcbd053d69002bd7f
Ren Maynard <120230052+renmaynard@users.noreply.github.com>
2023-09-20 17:40:54 -0600

    assignment 2

---
+++ app/student-info.js
@@ -0,0 +1,10 @@
+export function StudentInfo() {
+    (
+        <>
+        <p>Name: Rene Maynard</p>
+        <p>Course section: CPRG 306 E</p>
+        <Link href="https://github.com/renmaynard">https://github.com</Link>
+        </>
+    );
+    
+}
