279c1483b6f6dd734bcdb760a047157e64a3568b
Ren Maynard <120230052+renmaynard@users.noreply.github.com>
2023-10-15 18:05:32 -0600

    Add files via upload
    week 5 upload

---
+++ app/week5/item-list.js
@@ -0,0 +1,28 @@
+'use client';
+
+import {useState} from "react";
+import Item from "./item";
+import itemsData from "./items.json";
+
+export default function ItemList (){
+  const [sortBy, setSortBy] = useState("name");
+
+  if(sortBy == "name"){
+    itemsData.sort((a,b) => a.name.localeCompare(b.name));
+  }
+  if(sortBy == "category"){
+    itemsData.sort((a,b) => a.category.localeCompare(b.category));
+  }
+
+  return(
+  <>
+  <button value="name" onClick={(event) => {setSortBy(event.target.value)}} className={`rounded text-center border border-white w-24 h-10 ml-16 mt-5 ${sortBy == 'name' ? 'bg-blue-700' : 'bg-blue-950 hover:bg-blue-700'}`}>Name</button>
+  <button value="category" onClick={(event) => {setSortBy(event.target.value)}} className={`rounded text-center border border-white w-24 h-10 ml-9 mt-5 ${sortBy == 'category' ? 'bg-blue-700' : 'bg-blue-950 hover:bg-blue-700'}`}>Category</button>
+  <ul>
+    {itemsData.map((item) => (
+      <Item event={item} key={item.id} name={item.name} quantity={item.quantity} category={item.category}/>
+    ))}
+  </ul>
+  </>  
+  ); 
+}
